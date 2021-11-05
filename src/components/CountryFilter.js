5e879aac35233517e45c4886e7f97f860f6bf34f
dommentee <chuku0929@gmail.com>
2021-11-05 16:54:22 -0400

    did a lil css in front end

---
+++ src/components/CountryFilter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const dummyData = [
+  {
+    country: "Africa",
+  },
+  {
+    country: "Africa",
+  },
+  {
+    country: "China",
+  },
+  {
+    country: "Mexico",
+  },
+  
+]
+
+
+
--- src/components/ShowIndex.js
+++ src/components/ShowIndex.js
@@ -3,16 +3,19 @@ import '../css/post-card.css';
 
 const ShowIndex = (props) => {
   return (
-    <div>
+    <div className="container">
       <h2>Show Posts</h2>
       <div className="main-container">
         {
           props.posts.map((post) => {
             return (
               <div className="post-card">
+                <div className="post-info">
                 <h3>{post.title}</h3>
-                <h4>{post.country}</h4>
-                <h4>{post.city}</h4>
+                <span>Country: {post.country}</span>
+                <span>City: {post.city}</span>
+
+                </div>
                 <img src={post.image}/>
                 <p>{post.description}</p>
                 <button onClick={(e) => { props.handleDelete(post) }}>Delete</button>
